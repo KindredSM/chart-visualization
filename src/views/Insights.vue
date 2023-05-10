24f4d44245caf2313d527524a6648322b78540c9
Kindred <KDSM@protonmail.com>
2023-05-10 17:03:38 +0100

    chart

--- src/views/Insights.vue
+++ src/views/Insights.vue
@@ -2,26 +2,6 @@
   <div class="page">
     <h1>Bitcoin Price Index</h1>
 
-    <section v-if="errored">
-      <p>
-        We're sorry, we're not able to retrieve this information at the moment,
-        please try back later
-      </p>
-    </section>
-
-    <section v-else>
-      <div v-if="loading">Loading...</div>
-
-      <div v-else v-for="currency in info" class="currency">
-        {{ currency.description }}:
-        <span class="lighten">
-          <span v-html="currency.symbol"></span
-          >{{ currencyDecimal(currency.rate_float) }}
-        </span>
-      </div>
-    </section>
-
-    <h1>Managed Devices</h1>
     <div class="grid">
       <keep-alive>
         <draggable
